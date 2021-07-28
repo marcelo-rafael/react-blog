b9740fdc7b0bba522b75331ef1b3050c00c11b88
Marcelo Rafael Gonçalves <marcelo.rafael.goncalves@gmail.com>
2021-07-27 21:58:09 -0300

    Props e organização dos componentes - parte 1

---
+++ src/components/PostCard/index.jsx
@@ -0,0 +1,12 @@
+export function PostCard({ title, cover, body, id }) {
+  return (
+
+    <div key={id} className="post">
+      <img src={cover} alt={title} />
+      <div className="post-content" >
+        <h1>{title}</h1>
+        <p>{body}</p>
+      </div>
+    </div>
+  )
+}
